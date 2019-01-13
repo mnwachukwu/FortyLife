b45617334808ecfa987aecf1bccb70cdae1053ea
Matt N <matt.nwachukwu@gmail.com>
2019-01-13 15:16:38 -0500

    Updated about page

--- FortyLife/Controllers/HomeController.cs
+++ FortyLife/Controllers/HomeController.cs
@@ -15,16 +15,14 @@ namespace FortyLife.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
