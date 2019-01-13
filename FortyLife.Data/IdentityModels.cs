1c02a1e0daacc6ff526b5bfe6d1624cd78a27651
Matt N <matt.nwachukwu@gmail.com>
2019-01-13 18:25:23 -0500

    Added Core for main site logic and Data for EF and data operations; renamed base project to UI to handle all our front end

--- FortyLife.Data/IdentityModels.cs
+++ FortyLife.Data/IdentityModels.cs
@@ -1,10 +1,9 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace FortyLife.Models
+namespace FortyLife.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
--- FortyLife/App_Start/IdentityConfig.cs
+++ FortyLife/App_Start/IdentityConfig.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
+using FortyLife.Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
