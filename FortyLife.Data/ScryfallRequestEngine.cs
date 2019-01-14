ba8b330dbea4768e596e1f7674d892b6c5d1f138
Matt N <matt.nwachukwu@gmail.com>
2019-01-13 23:48:38 -0500

    Groundwork for a lot of Scryfall API implementation and EF

---
+++ FortyLife.Data/ScryfallRequestEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using FortyLife.Data.Scryfall;
+using Newtonsoft.Json;
+
+namespace FortyLife.Data
+{
+    public class ScryfallRequestEngine : RequestEngine
+    {
+        private const string BaseSearchUri = "https://api.scryfall.com/cards/search";
+
+        public ScryfallList Request(string requestUri)
+        {
+            var jsonResult = Get(BaseSearchUri + requestUri);
+            return JsonConvert.DeserializeObject<ScryfallList>(jsonResult);
+        }
+
+        public ScryfallList CardSearchRequest(string cardName)
+        {
+            return Request($"?q=name={cardName}");
+        }
+
+        public Card FirstCardFromSearch(string cardName)
+        {
+            var searchResultList = CardSearchRequest(cardName);
+            //TODO: Make sure we're returning a valid card or else return null
+            return searchResultList.Data.FirstOrDefault();
+        }
+    }
+}
