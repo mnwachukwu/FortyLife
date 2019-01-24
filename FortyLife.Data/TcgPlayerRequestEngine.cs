327c109036d33dc0f75d48b0ddc3c3b96c1c69f5
Matt N <matt.nwachukwu@gmail.com>
2019-01-23 23:46:33 -0500

    Beginnings of TCG Player request engine

--- FortyLife.Data/TcgPlayerRequestEngine.cs
+++ FortyLife.Data/TcgPlayerRequestEngine.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FortyLife.Data.TCGPlayer;
 
 namespace FortyLife.Data
 {
     public class TcgPlayerRequestEngine : RequestEngine
     {
+        public enum ProductCategory
+        {
+            Null = 0,
+            Mtg = 1
+        }
 
+        public const string ApiVersion = "v1.19.0";
+        public const string PublicKey = "5E4802A2-D073-4B82-B3E9-6A1783F5A099";
+
+        public string PrivateKey => string.Empty;
+
+        public void AccessTokenRequest()
+        {
+
+        }
+
+        public void StoreAccessToken()
+        {
+
+        }
+
+        public string ReadAccessToken()
+        {
+            return string.Empty;
+        }
+
+        public int ProductIdRequest(string cardName, string setName)
+        {
+            return 0;
+        }
+
+        public MarketPriceResults CardPriceRequest(int productId)
+        {
+            return new MarketPriceResults();
+        }
     }
 }
