45857ea8ed37bb77593d5d7b82beb0c29a5ce578
Matt N <matt.nwachukwu@gmail.com>
2019-02-16 11:03:17 -0500

    Implemented user authenticator class which also handles password encryption, removed custom attributes in place of existing ones to accomplish the same task

--- FortyLife.App/Controllers/AccountController.cs
+++ FortyLife.App/Controllers/AccountController.cs
@@ -5,23 +5,25 @@ namespace FortyLife.App.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
-
-        [RequireRequestValue("model")]
+        
+        [HttpPost]
         public ActionResult Login(LoginViewModel model)
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult Register()
         {
             return View();
         }
-
-        [RequireRequestValue("model")]
+        
+        [HttpPost]
         public ActionResult Register(RegisterViewModel model) // TODO: Account creation
         {
             return View();
