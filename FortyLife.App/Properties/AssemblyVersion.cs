6cd8908911f2ff14897191c9f47b381aefe8a9ff
Matt N <matt.nwachukwu@gmail.com>
2021-06-18 01:50:04 -0400

    Remove Web.config (since it is regenerated depending on build version anyway) and added AssemblyVersion template file
    Also removing the release config files with our secrets

---
+++ FortyLife.App/Properties/AssemblyVersion.cs
@@ -0,0 +1,14 @@
+﻿
+// This file is auto-generated and will be rebuilt automatically on each change.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Remove the properties defined below from the AssemblyInfo.cs file in the current project.
+// Use the definitions defined here to apply the standards.
+
+[assembly: AssemblyCopyright("Copyright © 2021 Studio TM14")]
+
+[assembly: AssemblyVersion("0.7.0.06181")]
+[assembly: AssemblyFileVersionAttribute("0.7.0.06181")]
